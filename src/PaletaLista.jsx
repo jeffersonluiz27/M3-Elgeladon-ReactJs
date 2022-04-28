6ddba75b31c22b8e4b0a77c152a3402e01fcc031
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-04-27 22:02:54 -0300

    badgecounter

--- src/PaletaLista.jsx
+++ src/PaletaLista.jsx
@@ -4,6 +4,15 @@ import { useState } from 'react';
 
 const PaletaLista = () => {
 	const [paletaSelecionada, setPaletaSelecionada] = useState({});
+
+	const badgeCounter = (canRender, index) =>
+		Boolean(canRender) && (
+			<span className="PaletaListaItem__badge">
+				{' '}
+				{paletaSelecionada[index]}{' '}
+			</span>
+		);
+
 	const adicionarItem = (paletaIndex) => {
 		const paleta = {
 			[paletaIndex]: Number(paletaSelecionada[paletaIndex] || 0) + 1,
@@ -15,10 +24,7 @@ const PaletaLista = () => {
 		<div className="PaletaLista">
 			{paletas.map((paleta, index) => (
 				<div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
-					<span className="PaletaListaItem__badge">
-						{' '}
-						{paletaSelecionada[index] || 0}{' '}
-					</span>
+					{badgeCounter(paletaSelecionada[index], index)}
 					<div>
 						<div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
 						<div className="PaletaListaItem__preco">
