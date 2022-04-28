a1d1ca1f17e70de153043a336c175ad12aecf614
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-04-27 23:10:44 -0300

    Paleta lista e paleta lista item

--- src/components/PaletaLista/PaletaLista.jsx
+++ src/components/PaletaLista/PaletaLista.jsx
@@ -1,24 +1,10 @@
 import './PaletaLista.css';
 import { paletas } from 'mocks/paletas.js';
 import { useState } from 'react';
+import PaletaListaItem from 'components/PaletaListaItem/PaletaListaItem';
 
 const PaletaLista = () => {
 	const [paletaSelecionada, setPaletaSelecionada] = useState({});
-
-	const badgeCounter = (canRender, index) =>
-		Boolean(canRender) && (
-			<span className="PaletaListaItem__badge">
-				{' '}
-				{paletaSelecionada[index]}{' '}
-			</span>
-		);
-
-	const removeButton = (canRender, index) =>
-		Boolean(canRender) && (
-			<button className="Acoes__remover" onClick={() => removerItem(index)}>
-				remover
-			</button>
-		);
 
 	const adicionarItem = (paletaIndex) => {
 		const paleta = {
@@ -37,36 +23,7 @@ const PaletaLista = () => {
 	return (
 		<div className="PaletaLista">
 			{paletas.map((paleta, index) => (
-				<div className="PaletaListaItem" key={`PaletaListaItem-${index}`}>
-					{badgeCounter(paletaSelecionada[index], index)}
-					<div>
-						<div className="PaletaListaItem__titulo"> {paleta.titulo} </div>
-						<div className="PaletaListaItem__preco">
-							{' '}
-							R$ {paleta.preco.toFixed(2)}{' '}
-						</div>
-						<div className="PaletaListaItem__descricao">
-							{' '}
-							{paleta.descricao}{' '}
-						</div>
-						<div className="PaletaListaItem__acoes Acoes">
-							<button
-								className={`Acoes__adicionar ${
-									!paletaSelecionada[index] && 'Acoes__adicionar--preencher'
-								}`}
-								onClick={() => adicionarItem(index)}
-							>
-								adicionar
-							</button>
-							{removeButton(paletaSelecionada[index], index)}
-						</div>
-					</div>
-					<img
-						className="PaletaListaItem__foto"
-						src={paleta.foto}
-						alt={`Paleta de ${paleta.sabor}`}
-					/>
-				</div>
+				<PaletaListaItem key={`PaletaListaItem-${index}`} />
 			))}
 		</div>
 	);
