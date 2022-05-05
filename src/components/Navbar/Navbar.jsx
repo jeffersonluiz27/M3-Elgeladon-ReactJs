59a24eeeb23c3010cad25f333adb18dcb9818db4
jeffersonluiz27 <developer.jefferson27@outlook.com>
2022-05-05 20:17:13 -0300

    add menu atualizar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -3,8 +3,9 @@ import './Navbar.css';
 import sacola from 'assets/icons/sacola.svg';
 import logo from 'assets/logo-el-geladon.png';
 import paleta from 'assets/icons/paleta.svg';
+import atualizar from 'assets/icons/atualizar.svg';
 
-function Navbar({ createPaleta }) {
+function Navbar({ createPaleta, updatePaleta }) {
 	return (
 		<div className="Home__header Header">
 			<div className="row">
@@ -18,6 +19,18 @@ function Navbar({ createPaleta }) {
 					<span className="Logo__titulo"> El Geladon </span>
 				</div>
 				<div className="Header__opcoes Opcoes">
+					<button
+						type="button"
+						className="Opcoes__paleta Paleta"
+						onClick={() => updatePaleta()}
+					>
+						<img
+							src={atualizar}
+							width="40px"
+							className="Paleta__icone"
+							alt="Editar paleta"
+						/>
+					</button>
 					<button
 						type="button"
 						className="Opcoes__paleta Paleta"
